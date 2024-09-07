c65714e54204568e9aa8e9cc0ef527a27967fb03
Raphael Maurício <raphaelmauricio12@gmail.com>
2024-09-07 15:13:26 -0300

    [FEAT] add conteiner DependencyInjection

--- TesteTecnicoTechNation.Infra.IoC/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TesteTecnicoTechNation.Infra.IoC
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ TesteTecnicoTechNation.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using TesteTecnicoTechNation.Domain.Interfaces.Repositories;
+using TesteTecnicoTechNation.Application.Interfaces;
+using TesteTecnicoTechNation.Application.Services;
+using TesteTecnicoTechNation.Application.Mappings;
+using TesteTecnicoTechNation.Infra.Data.Repositories;
+
+namespace TesteTecnicoTechNation.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            {
+                services.AddTransient<IDbConnection>(sp =>
+                    new SqlConnection(configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddScoped<INotaFiscalRepository, NotaFiscalRepository>();
+                services.AddScoped<INotaFiscalService, NotaFiscalService>();
+
+                services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+                return services;
+            }
+        }
+    }
+}
